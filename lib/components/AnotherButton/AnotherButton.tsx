dcf044876ef9137d979b25a03d5654eb15377775
Raul Argueta <real-io@noreply.users.github.com>
2024-01-31 23:06:48 -0600

    Updating comments

--- lib/components/AnotherButton/AnotherButton.tsx
+++ lib/components/AnotherButton/AnotherButton.tsx
@@ -4,7 +4,9 @@ interface AnotherButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant: string;
 }
-
+/**
+ * This is another button component
+ */
 const AnotherButton = ({ variant, children, ...props }: AnotherButtonProps) => {
   return (
     <button data-variant={variant} className={styles["anotherButton"]} {...props}>
