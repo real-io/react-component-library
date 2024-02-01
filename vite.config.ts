09375b348633d0fde9d913c33a94c46481926282
Raul Argueta <real-io@noreply.users.github.com>
2024-01-31 22:09:45 -0600

    File splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { resolve, extname, relative } from "path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 
@@ -15,6 +17,20 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        glob.sync("lib/**/*.{ts,tsx}").map((file) => [
+          // The name of the entry point
+          // lib/nested/foo.ts becomes nested/foo
+          relative("lib", file.slice(0, file.length - extname(file).length)),
+          // The absolute path tot he entry file
+          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+          fileURLToPath(new URL(file, import.meta.url)),
+        ])
+      ),
+      output: {
+        assetFileNames:'assets/[name][extname]',
+        entryFileNames: '[name].js'
+      },
     },
     copyPublicDir: false,
   },
