79c31d875a52725871ff050db507d6b81e99dc20
Raul Argueta <real-io@noreply.users.github.com>
2024-01-31 22:50:58 -0600

    Adding comments

--- lib/components/Button/Button.tsx
+++ lib/components/Button/Button.tsx
@@ -4,6 +4,9 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant: string;
 }
 
+/**
+ * This is a button element
+ */
 const Button = ({ variant, children, ...props }: ButtonProps) => {
   return (
     <button data-variant={variant} className={styles["button"]} {...props}>
